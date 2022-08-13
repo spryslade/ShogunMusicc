35c55cf3ef8e998e95c266736b556adf96a09445
ʙᴏʙʙʏ <109764682+love-u-bobby@users.noreply.github.com>
2022-08-13 23:28:50 +0530

    BOBBY

---
+++ Bobby/config.py
@@ -0,0 +1,35 @@
+from os import getenv
+from dotenv import load_dotenv
+
+load_dotenv()
+
+que = {}
+admins = {}
+SESSION_NAME = getenv("SESSION_NAME", "session")
+BOT_TOKEN = getenv("BOT_TOKEN")
+BOT_NAME = getenv("BOT_NAME")
+BOT_USERNAME = getenv("BOT_USERNAME")
+ASSID = int(getenv("ASSID"))
+ASSNAME = getenv("ASSNAME")
+ASSUSERNAME = getenv("ASSUSERNAME")
+BOT_ID = int(getenv("BOT_ID"))
+UPSTREAM_REPO = getenv("UPSTREAM_REPO")
+UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH")
+HEROKU_API_KEY = getenv("HEROKU_API_KEY")
+HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
+MONGO_DB_URI = getenv("MONGO_DB_URI")
+API_ID = int(getenv("API_ID"))
+API_HASH = getenv("API_HASH")
+OWNER_ID = int(getenv("OWNER_ID", "5448900586"))
+UPDATE = getenv("UPDATE", "COMRADE_ROBOTZ")
+SUPPORT = getenv("SUPPORT", "COMRADE_BOTZ")
+DURATION_LIMIT = int(getenv("DURATION_LIMIT", "9999"))
+CMD_MUSIC = list(getenv("CMD_MUSIC", "/ !").split())
+BG_IMG = getenv("BG_IMG", "https://telegra.ph/file/1bab5e1b04d56eb02e09f.jpg")
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "5448900586").split()))
+START_PIC = getenv("START_PIC", "https://telegra.ph/file/1bab5e1b04d56eb02e09f.jpg")
+OWNER_USERNAME = getenv("OWNER_USERNAME")
+IMG_1 = getenv("IMG_1", "https://telegra.ph/file/1bab5e1b04d56eb02e09f.jpg")
+IMG_2 = getenv("IMG_2", "https://telegra.ph/file/82d11d4e716285ad977d6.jpg")
+IMG_3 = getenv("IMG_3", "https://telegra.ph/file/a48b45f831a7cd68f894b.jpg")
+IMG_4 = getenv("IMG_4", "https://telegra.ph/file/98d15908bef394edacfa0.jpg")
