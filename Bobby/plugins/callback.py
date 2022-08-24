330574844a3b0d42855c9a7f96d6008b0b16ae75
XDHate#Ded <98488953+XhateXd@users.noreply.github.com>
2022-08-24 16:20:53 +0530

    Update callback.py

--- Bobby/plugins/callback.py
+++ Bobby/plugins/callback.py
@@ -7,17 +7,14 @@ from Bobby.config import BOT_NAME, OWNER_USERNAME, UPDATE, SUPPORT, BOT_USERNAME
 @Client.on_callback_query(filters.regex("home"))
 async def home(_, query: CallbackQuery):
     await query.edit_message_text(
-        f"""━━━━━━━━━━━━━━━━━━
-ʜᴇʏ ❣️!
+        f"""ʜᴇʏ !
 ᴛʜɪs ɪs ʀᴀɪᴅᴇɴ ᴍᴜsɪᴄ sʏsᴛᴇᴍ ᴡɪᴛʜ ᴀ ʟᴏᴛ ᴏꜰ ꜰᴇᴀᴛᴜʀᴇꜱ ʙᴀꜱᴇᴅ ᴏɴ ᴀ.ɪ ᴀɴᴅ ʜɪɢʜ ꜱᴏᴜɴᴅ ǫᴜᴀʟɪᴛʏ ᴏꜰ ꜱᴏɴɢꜱ.
-━━━━━━━━━━━━━━━━━━""",
+""",
     reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(
-                        "🗂 ᴄᴏᴍᴍᴀɴᴅs", callback_data="cmds"),
-                    InlineKeyboardButton(
-                        "🆘 ʜᴇʟᴘ", url=f"https://t.me/{SUPPORT}")
+                        "🗂 ᴄᴏᴍᴍᴀɴᴅs", callback_data="cmds")                    
                 ]                                
            ]
         ),
