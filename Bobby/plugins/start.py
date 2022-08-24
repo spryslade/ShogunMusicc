5836067195342d38039e5a3b2ac66e718fc52b41
XDHate#Ded <98488953+XhateXd@users.noreply.github.com>
2022-08-24 16:19:55 +0530

    Update start.py

--- Bobby/plugins/start.py
+++ Bobby/plugins/start.py
@@ -25,17 +25,14 @@ TIME_DURATION_UNITS = (
 @Client.on_message(command("start") & filters.private & ~filters.edited)
 async def start_(client: Client, message: Message):
     await message.reply_text(
-        f"""━━━━━━━━━━━━━━━━━━
-ʜᴇʏ ❣️{message.from_user.mention()} 😿
+        f"""ʜᴇʏ {message.from_user.mention()} 😿
 ᴛʜɪs ɪs ʀᴀɪᴅᴇɴ ᴍᴜsɪᴄ sʏsᴛᴇᴍ ᴡɪᴛʜ ᴀ ʟᴏᴛ ᴏꜰ ꜰᴇᴀᴛᴜʀᴇꜱ ʙᴀꜱᴇᴅ ᴏɴ ᴀ.ɪ ᴀɴᴅ ʜɪɢʜ ꜱᴏᴜɴᴅ ǫᴜᴀʟɪᴛʏ ᴏꜰ ꜱᴏɴɢꜱ.
-━━━━━━━━━━━━━━━━━━""",
+""",
     reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(
-                        "🗂 ᴄᴏᴍᴍᴀɴᴅs", callback_data="cmds"),
-                    InlineKeyboardButton(
-                        "🆘 ʜᴇʟᴘ", url=f"https://t.me/{SUPPORT}")
+                        "🗂 ᴄᴏᴍᴍᴀɴᴅs", callback_data="cmds")                    
                 ]                               
            ]
         ),
